3d85bbf2dd493f4a5227c92ecc18dae54afe0f4f
Awab <awab.qureshi@gmail.com>
2022-07-09 19:58:27 +0500

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import { Logo } from './logo'
+
+export function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+const options = [
+      'Rock',
+      'Paper',
+      'Scissor'
+    ]
+
+const rand = (Math.random()*10) % 3
+
+const rockPaper = () => {
+
+}
