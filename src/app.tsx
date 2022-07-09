a897d9c5317eb4b0f0769cde5b68b5ea2b9545e1
Awab <awab.qureshi@gmail.com>
2022-07-09 20:59:47 +0500

    Added result styling

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import './app.css'
 
 export function App() {
   return (
+  <>
     <div class="flexy">
         <div class="col-container">
             <button class='button'>🪨</button>
@@ -15,8 +16,29 @@ export function App() {
             <button class='button'>✂️</button>
         </div>
     </div>
+
+    <h1 class="result">{result}</h1>
+
+    <div class="flexy" >
+        <span style={{margin:"0.5em"}}>
+            <h1>🖥 </h1>
+            <div class='button'>{computerAns}</div>
+        </span>
+
+        <span >
+            <h1>  🙋‍♀️</h1>
+            <div class='button'>{humanAns}</div>
+        </span>
+    </div>
+  </>
   )
 }
+
+const computerAns: string = "?";
+
+const humanAns: string = "?";
+
+const result: string = "TIE";
 
 const options = [
       'Rock',
