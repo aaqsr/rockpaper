bdfaa0253cdb61652238b965a597cbbb55c8ad6d
Awab <awab.qureshi@gmail.com>
2022-07-09 20:37:05 +0500

    Removed Headings

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,14 @@ export function App() {
   return (
     <div class="flexy">
         <div class="col-container">
-            <h1>ROCK</h1>
             <button class='button'>🪨</button>
         </div>
 
         <div class="col-container">
-            <h1>PAPER</h1>
             <button class='button'>🗞</button>
         </div>
 
         <div class="col-container">
-            <h1>SCISSOR</h1>
             <button class='button'>✂️</button>
         </div>
     </div>
